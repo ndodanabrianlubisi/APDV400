864a8cc8d4687b25612eda1398f143f899dfd097
Lubisi 201743566 <201743566@ump.ac.za>
2021-05-14 15:05:17 +0200

    apd400

---
+++ src/add/ADD.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package add;
+
+/**
+ *
+ * @author 201743566
+ */
+public class ADD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int a =20;
+        int b =30;
+        int c = a+b;
+         System.out.println("a = 20 + b =30 is equal to"+c);
+    }
+    
+}
